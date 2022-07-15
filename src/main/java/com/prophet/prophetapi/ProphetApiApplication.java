f67af77339453aa3e5f07da60d6cafbbcddf7805
zcmace <zcmace@gmail.com>
2022-07-15 02:14:41 -0400

    init spring

---
+++ src/main/java/com/prophet/prophetapi/ProphetApiApplication.java
@@ -0,0 +1,13 @@
+package com.prophet.prophetapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProphetApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProphetApiApplication.class, args);
+	}
+
+}
