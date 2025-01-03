b053ed525bbeef83f910a3273a85bc52210f1f1e
Zachary Mace <zcmace@gmail.com>
2025-01-03 16:23:08 -0500

    fixed test and build results

--- src/test/java/com/prophet/prophetapi/controllers/PropertyControllerTest.java
+++ src/test/java/com/prophet/prophetapi/controllers/PropertyControllerTest.java
@@ -1,51 +1,43 @@
 package com.prophet.prophetapi.controllers;
 
-import com.prophet.prophetapi.services.PropertyService;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.prophet.prophetapi.services.PropertyService;
 import java.util.HashMap;
 import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-@ExtendWith(MockitoExtension.class)
+@WebMvcTest(PropertyController.class)
 public class PropertyControllerTest {
 
-    @Mock
-    private PropertyService propertyService;
+  @MockBean
+  private PropertyService propertyService;
 
-    @InjectMocks
-    private PropertyController propertyController;
+  @Autowired
+  private MockMvc mockMvc;
 
-    private MockMvc mockMvc;
+  @BeforeEach
+  public void setup() {
+    Map<String, String> properties = new HashMap<>();
+    properties.put("key", "value");
 
-    @BeforeEach
-    public void setup() {
-        mockMvc = MockMvcBuilders.standaloneSetup(propertyController).build();
+    when(propertyService.getAllProperties()).thenReturn(properties);
+  }
 
-        Map<String, String> properties = new HashMap<>();
-        properties.put("key", "value");
-
-        when(propertyService.getAllProperties()).thenReturn(properties);
-    }
-
-    @Test
-    public void testGetProperties() throws Exception {
-        mockMvc.perform(get("/property")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().json("{\"key\":\"value\"}"));
-    }
-
+  @Test
+  public void testGetProperties() throws Exception {
+    mockMvc.perform(get("/property")
+            .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(content().json("{\"key\":\"value\"}"));
+  }
 }
