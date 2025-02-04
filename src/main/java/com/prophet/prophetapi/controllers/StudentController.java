afb9eebeb53de6d5b52be45546051dd8c00ca939
Zachary Mace <zcmace@gmail.com>
2025-02-04 03:41:47 -0500

    added checkstyle enforcement for warnings, run checkstyle on build by default as validation

--- src/main/java/com/prophet/prophetapi/controllers/StudentController.java
+++ src/main/java/com/prophet/prophetapi/controllers/StudentController.java
@@ -2,18 +2,13 @@ package com.prophet.prophetapi.controllers;
 
 import com.prophet.prophetapi.dto.StudentDto;
 import com.prophet.prophetapi.dto.StudentSearchRequest;
-import com.prophet.prophetapi.model.Student;
 import com.prophet.prophetapi.services.StudentService;
-import java.util.List;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,6 +30,4 @@ public class StudentController {
   private Page<StudentDto> searchStudents(@RequestBody StudentSearchRequest studentSearchRequest) {
     return studentService.searchStudents(studentSearchRequest);
   }
-
-
 }
